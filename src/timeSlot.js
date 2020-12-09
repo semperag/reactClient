1fa895e8177f9f8f4220d19b1493e9912bf00844
AdiSemper <semperag@dukes.jmu.edu>
2020-12-08 19:40:11 -0500

    commit

--- src/timeSlot.js
+++ src/timeSlot.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 export function TimeSlot(props) {
     const slot = props.slot;
-    const startTime = (25 - parseInt(slot.start)) * 30 ;
-    const endTime = (25 - parseInt(slot.end)) * 30 ;
-    const heightNumber = startTime - endTime - 2;
+    let startTime = (24 - parseInt(slot.start)) * 50 + 2;
+    const endTime = (24 - parseInt(slot.end)) * 50 ;
+    const heightNumber = startTime - endTime - 4;
+    let looper = heightNumber;
+
+    while (looper > 46) {
+        startTime = startTime - 50;
+        looper = looper - 50;
+    }
+    startTime = startTime + 50;
 
     console.log(startTime + '   -   ' + endTime);
 
