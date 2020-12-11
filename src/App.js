cc0fffc1591879c6f7751c64ca6f2eeb27caca17
AdiSemper <semperag@dukes.jmu.edu>
2020-12-11 14:23:43 -0500

    commut

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ weekday[6]="Sunday";
 function App() {
 
   const [start, setStart] = useState(1);
-  const [end, setEnd] = useState(1);
+  const [end, setEnd] = useState(2);
   const [message, setMessage] = useState('');
 
   const memories = useSelector(state => state.memories);
@@ -52,72 +52,73 @@ return (
   <div className="memories-root">
     <div id="app-name">Calendar</div>
     <div id="calendar-day">
-
-
-
       
     <div id="calendar-times">
         <div id="time-selector">
-            <select id="first-times"
-                value={start}
-                onChange={e => setStart(parseInt(e.target.value))}
-            >
-                <option value="1">12:00am</option>
-                <option value="2">1:00am</option>
-                <option value="3">2:00am</option>
-                <option value="4">3:00am</option>
-                <option value="5">4:00am</option>
-                <option value="6">5:00am</option>
-                <option value="7">6:00am</option>
-                <option value="8">7:00am</option>
-                <option value="9">8:00am</option>
-                <option value="10">9:00am</option>
-                <option value="11">10:00am</option>
-                <option value="12">11:00am</option>
-                <option value="13">12:00pm</option>
-                <option value="14">1:00pm</option>
-                <option value="15">2:00pm</option>
-                <option value="16">3:00pm</option>
-                <option value="17">4:00pm</option>
-                <option value="18">5:00pm</option>
-                <option value="19">6:00pm</option>
-                <option value="20">7:00pm</option>
-                <option value="21">8:00pm</option>
-                <option value="22">9:00pm</option>
-                <option value="23">10:00pm</option>
-                <option value="24">11:00pm</option>
-            </select>
+            <span className="selector">
+              <select id="first-times"
+                  value={start}
+                  onChange={e => setStart(parseInt(e.target.value))}
+              >
+                  <option value="1">12:00am</option>
+                  <option value="2">1:00am</option>
+                  <option value="3">2:00am</option>
+                  <option value="4">3:00am</option>
+                  <option value="5">4:00am</option>
+                  <option value="6">5:00am</option>
+                  <option value="7">6:00am</option>
+                  <option value="8">7:00am</option>
+                  <option value="9">8:00am</option>
+                  <option value="10">9:00am</option>
+                  <option value="11">10:00am</option>
+                  <option value="12">11:00am</option>
+                  <option value="13">12:00pm</option>
+                  <option value="14">1:00pm</option>
+                  <option value="15">2:00pm</option>
+                  <option value="16">3:00pm</option>
+                  <option value="17">4:00pm</option>
+                  <option value="18">5:00pm</option>
+                  <option value="19">6:00pm</option>
+                  <option value="20">7:00pm</option>
+                  <option value="21">8:00pm</option>
+                  <option value="22">9:00pm</option>
+                  <option value="23">10:00pm</option>
+                  <option value="24">11:00pm</option>
+              </select>
+            </span>
 
             <span> - </span>
 
-            <select id="second-times"
-                value={end}
-                onChange={e => setEnd(parseInt(e.target.value))}
-            >
-                <option value="2">1:00am</option>
-                <option value="3">2:00am</option>
-                <option value="4">3:00am</option>
-                <option value="5">4:00am</option>
-                <option value="6">5:00am</option>
-                <option value="7">6:00am</option>
-                <option value="8">7:00am</option>
-                <option value="9">8:00am</option>
-                <option value="10">9:00am</option>
-                <option value="11">10:00am</option>
-                <option value="12">11:00am</option>
-                <option value="13">12:00pm</option>
-                <option value="14">1:00pm</option>
-                <option value="15">2:00pm</option>
-                <option value="16">3:00pm</option>
-                <option value="17">4:00pm</option>
-                <option value="18">5:00pm</option>
-                <option value="19">6:00pm</option>
-                <option value="20">7:00pm</option>
-                <option value="21">8:00pm</option>
-                <option value="22">9:00pm</option>
-                <option value="23">10:00pm</option>
-                <option value="24">11:00pm</option>
-            </select>
+            <span className="selector">
+              <select id="second-times"
+                  value={end}
+                  onChange={e => setEnd(parseInt(e.target.value))}
+              >
+                  <option value="2">1:00am</option>
+                  <option value="3">2:00am</option>
+                  <option value="4">3:00am</option>
+                  <option value="5">4:00am</option>
+                  <option value="6">5:00am</option>
+                  <option value="7">6:00am</option>
+                  <option value="8">7:00am</option>
+                  <option value="9">8:00am</option>
+                  <option value="10">9:00am</option>
+                  <option value="11">10:00am</option>
+                  <option value="12">11:00am</option>
+                  <option value="13">12:00pm</option>
+                  <option value="14">1:00pm</option>
+                  <option value="15">2:00pm</option>
+                  <option value="16">3:00pm</option>
+                  <option value="17">4:00pm</option>
+                  <option value="18">5:00pm</option>
+                  <option value="19">6:00pm</option>
+                  <option value="20">7:00pm</option>
+                  <option value="21">8:00pm</option>
+                  <option value="22">9:00pm</option>
+                  <option value="23">10:00pm</option>
+                  <option value="24">11:00pm</option>
+              </select>
+            </span>
             <textarea value={message} onChange={e =>setMessage(e.target.value)}/>
             <button id="insert" onClick={onAdd}>New Event</button>
         </div>
