14cc55100b803454fc20eee7a1075ddbb0edffa4
AdiSemper <semperag@dukes.jmu.edu>
2020-12-11 13:32:37 -0500

    com

--- src/App.js
+++ src/App.js
@@ -9,7 +9,20 @@ const year = date.getFullYear();
 const month = date.getMonth() + 1;
 const day = date.getDate();
 
+var weekday = new Array(7);
+weekday[0]="Monday";
+weekday[1]="Tuesday";
+weekday[2]="Wednesday";
+weekday[3]="Thursday";
+weekday[4]="Friday";
+weekday[5]="Saturday";
+weekday[6]="Sunday";
+
 function App() {
+
+  const [start, setStart] = useState(1);
+  const [end, setEnd] = useState(1);
+  const [message, setMessage] = useState('');
 
   const memories = useSelector(state => state.memories);
   const dispatch = useDispatch();
@@ -18,14 +31,10 @@ function App() {
     dispatch(loadDay(month, day));
   }, [dispatch]);
 
-  const onAdd = () => {
-    dispatch(startAddingMemory(year, month, day));
-  }
-
-  const addSlot = newSlot => {
-    setSlots(timeSlots => [newSlot, ...timeSlots.filter(
-    slot => slot.start !== newSlot.start)]);
-  }
+  let startTime = (24 - parseInt(start)) * 50;
+  let endTime = (24 - parseInt(end)) * 50 ;
+  let heightNumber = startTime - endTime - 4;
+  let looper = heightNumber;
 
 
   //return (
